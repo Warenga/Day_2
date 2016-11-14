798ae34c609660b70703088f9621675f1f7e1142
Warenga <warengam@gmail.com>
2016-11-14 15:00:09 +0300

    Typo error

--- cat_facts_cli/catfacts.py
+++ cat_facts_cli/catfacts.py
@@ -15,19 +15,6 @@ def main():
 	Enjoy :)
 
 	"""
-
-	# no_of_facts = raw_input('How many facts do you want? ')
-
-	# if no_of_facts == '':
-	# 	no_of_facts = '0' 
-	# try: 
-	# 	if int(no_of_facts) > 100:
-	# 		print 'Woah!! too many facts. Can only display maximum of 100 facts'
-	# 		no_of_facts = raw_input('Please try again. How many facts do you want?')
-	# except:
-	# 	print "Oh Oh...That's not a number"
-	# 	no_of_facts = raw_input('Please try again. How many facts do you want?')
-
 	while True:
 		no_of_facts = raw_input('How many facts do you want? ')
 		if no_of_facts == '':
@@ -60,7 +47,7 @@ def main():
 				file = open('catfacts.txt', 'wb')
 				file.write(catfacts)
 				file.close()
-				print 'Your facts has been saved as catfacts.txt'
+				print 'Your facts have been saved as catfacts.txt'
 			except URLError, e:
 				print 'Error code:',e
 		else:
