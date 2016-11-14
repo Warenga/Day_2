335d95cfaccd7354b52f078bb487c46926e7678b
Warenga <warengam@gmail.com>
2016-11-14 10:59:02 +0300

    test_find_max_min_identity passed

--- Andela_labs/max_and_min/max_min.py
+++ Andela_labs/max_and_min/max_min.py
@@ -7,4 +7,7 @@ def find_max_min(numbers):
 			max_num = num
 		elif num < min_num:
 			min_num = num
-	return [min_num, max_num]
+	if min_num == max_num:
+		return [len(numbers)]
+	else:
+		return [min_num, max_num]
